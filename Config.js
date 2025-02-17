fcd5cc72c40c1500b4848e3814078de420f6fadb
mrgozzy <realkeanureeves0208@gmail.com>
2025-02-17 19:20:37 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "1234564789", // your number with country code
-  OWNER_NAME: 'シ', // your owner name 
+  OWNER_NUMBERS: "07049920895", // your number with country code
+  OWNER_NAME: 'big Gozzy', // your owner name 
   BOT_NAME: 'Kord AI', // your preferred bot name
-  SESSION_ID: '', // replace your_sessionid with yours and make sure its inside the 
+  SESSION_ID: 'kord_ai-MhBMUSHWIcbg', // replace your_sessionid with yours and make sure its inside the 
   INVINCIBLE_MODE: true, // toogle invincible mode 
   WORK_MODE: 'public',
   TIME_ZONE: "Africa/Lagos",
@@ -18,11 +18,11 @@ global.settings = {
   MAX_COMMANDS_PER_MINUTE: 100,
   MAX_DOWNLOAD_SIZE: 100,
   AUTO_LIKE_STATUS: true,
-  AUTO_LIKE_EMOJI: '✨',
+  AUTO_LIKE_EMOJI: '😐',
   BANKING_DETAILS: {
-    bank: "Kord-Ai Bank", // the name of your preferred bank
-    acc_number: "1234567890", // account number for your preferred bank
-    acc_name: "MyAccountName" // name of your bank
+    bank: "moniepoint", // the name of your preferred bank
+    acc_number: "7049920895", // account number for your preferred bank
+    acc_name: "Godwin Paul" // name of your bank
   },
   /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
   API_KEYS: {
@@ -30,7 +30,7 @@ global.settings = {
   },
   isHackEnable: true,
   SAFE_SEARCH: true,
-  REJECT_CALLS: true,
+  REJECT_CALLS: false,
   SEND_WELCOME_MESSAGE: false,
   WELCOME_MESSAGE: "👋 Welcome to the group! We're glad to have you here. 😊",
   DELETE_WA_LINKS: false,
@@ -39,8 +39,8 @@ global.settings = {
   ANTI_EDIT_IN_CHAT: false,
   ANTI_PM_SPAM: false, //pm permit 
   ANTI_PM_SPAM_COUNT: 6,
-  AUTO_TYPING: false, // if you wnat the bot to be typing always 
-  AUTO_RECORDING: false, //if you want the bot to be recording always
-  ALWAYS_ONLINE: false,
+  AUTO_TYPING: true, // if you wnat the bot to be typing always 
+  AUTO_RECORDING: true, //if you want the bot to be recording always
+  ALWAYS_ONLINE: true,
 
 };
